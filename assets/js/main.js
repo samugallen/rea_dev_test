e891673af1a7713b0724db057dfb144f1badea8e
samugallen <samuel.geoffrey.allen@gmail.com>
2018-05-07 18:21:06 +1000

    Added controls for adding and removing of properties.

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,57 @@ var devTest = {
     // Loads the JSON from the specified location and saves it to the prepared variable
     $.getJSON( "data/properties.json", function( data ) {
       propertyData = data;
+      devTest.displayProperties(propertyData);
+    });
+  },
+  displayProperties: function(propertyData) {
+    // Loop through results and display the properties in the results column
+    for (i = 0; i < propertyData.results.length; i++) {
+      devTest.propertyTemplate("#results", propertyData.results[i]);
+    }
+    // Loop through results and display the properties in the saved column
+    for (i = 0; i < propertyData.results.length; i++) {
+      devTest.propertyTemplate("#saved-properties", propertyData.saved[i]);
+    }
+    // Prepare property button functions
+    devTest.propertyButtonControls();
+  },
+  propertyTemplate: function(target, property) {
+    if (property != undefined || property != null) {
+      var propertyHTML =  "<div data-propertyID='property-" + property.id + "' class='property'>";
+      propertyHTML +=       "<div class='property-header' style='background: " + property.agency.brandingColors.primary + ";'>";
+      propertyHTML +=         "<img src='" + property.agency.logo + "' alt='' class='property-logo'/>";
+      propertyHTML +=       "</div>";
+      propertyHTML +=       "<div class='property-body'>";
+      propertyHTML +=         "<img src='" + property.mainImage + "' alt='' class='main-image'/>";
+      propertyHTML +=         "<a href='#' class='property-control-button'><span class='add'>Add Property</span><span class='remove'>Remove Property</span></a>";
+      propertyHTML +=       "</div>";
+      propertyHTML +=       "<div class='property-footer'>";
+      propertyHTML +=         "<span class='price'>" + property.price + "</span>";
+      propertyHTML +=       "</div>";
+      propertyHTML +=      "</div>";
+
+      $(target).append(propertyHTML);
+    }
+  },
+  propertyButtonControls: function() {
+    $('#results .property-control-button').on("click", function(e) {
+      e.preventDefault();
+      // Check if the property is already saved.
+      var propertyID = $(this).closest(".property").attr("data-propertyID");
+      if ($("#saved-properties .property[data-propertyID=" + propertyID + "]").length <= 0) {
+        // Add the property to the saved list.
+        $(this).closest(".property").clone().appendTo('#saved-properties');
+        // Disable the button and update its text.
+        $(this).addClass("disabled").find('span.add').html("Saved")
+      }
+    });
+    $(document).on("click", "#saved-properties .property-control-button", function() {
+      var propertyID = $(this).closest(".property").attr("data-propertyID");
+      // Remove the property
+      $(this).closest(".property").remove();
+      // Enable the button in the results column and update its text.
+      $("#results .property[data-propertyID=" + propertyID + "] .property-control-button").removeClass("disabled").find('span.add').html("Add Property")
     });
   }
 }
--- index.php
+++ index.php
@@ -3,20 +3,10 @@
   <section id="properties-list">
     <div class="container">
       <div id="results" class="column">
-        <div class="property">
-          <div class="property-header">
-            <img src="http://i1.au.reastatic.net/agencylogo/XRWXMT/12/20120927204448.gif" alt="" class="property-logo"/>
-          </div>
-          <div class="property-image">
-            <img src="http://i2.au.reastatic.net/640x480/20bfc8668a30e8cabf045a1cd54814a9042fc715a8be683ba196898333d68cec/main.jpg" alt="" class="main-image"/>
-          </div>
-          <div class="property-footer">
-            <span class="price">$725,500</span>
-          </div>
-        </div>
+        <h2>Results</h2>
       </div>
       <div id="saved-properties" class="column">
-
+        <h2>Saved Properties</h2>
       </div>
     </div>
   </section>
